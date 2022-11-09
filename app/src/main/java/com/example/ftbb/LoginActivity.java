4315f7401b3c6edc3aba7b7aaed709c7b6e4449a
MalekHm <malek.hamhoum@esprit.tn>
2022-11-09 23:39:01 +0100

    init project (login + inscri interface)

---
+++ app/src/main/java/com/example/ftbb/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.ftbb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button Register, Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Login = findViewById(R.id.cnx);
+        Register = findViewById(R.id.inscri);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, InscriActivity.class);
+                startActivity(intent);
+            }
+        });
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
